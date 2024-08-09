ea13b8cba0939b9cc1737c902043f0f5453c3b7a
Prasun2004 <166635147+Prasun2004@users.noreply.github.com>
2024-08-09 17:15:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
-import Button from '@mui/material/Button';
-import SearchBox from './SearchBox';
-import InfoBox from './InfoBox';
 import WeatherApp from './WeatherApp';
 
 
